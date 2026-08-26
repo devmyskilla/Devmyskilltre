48f590334b315a48437da6228779c447e76b30d1
Aasim Mohammed Altomi <atomy8774@gmail.com>
2026-08-26 05:07:09 +0300

    feat: add public Supabase client configuration

---
+++ js/supabase-config.js
@@ -0,0 +1,5 @@
+// Public client configuration. Supabase publishable keys are designed to be exposed in browser apps when RLS is enabled.
+var SUPABASE_CONFIG = Object.freeze({
+  projectUrl: 'https://pptjaflkltwavyktzphw.supabase.co',
+  publishableKey: 'sb_publishable_lUq2yPmkTijaqyYDifTYFA_Es-Xi7Nb'
+});
